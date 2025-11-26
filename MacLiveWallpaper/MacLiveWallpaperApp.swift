84845ab5a10cb5c5ab50802f6358656dd195b8cf
HONGMIN LI <li.hongmin.xa@alumni.tsukuba.ac.jp>
2025-11-26 15:43:15 +0900

    Initial Commit

---
+++ MacLiveWallpaper/MacLiveWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLiveWallpaperApp.swift
+//  MacLiveWallpaper
+//
+//  Created by Li Hongmin on 2025/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MacLiveWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
